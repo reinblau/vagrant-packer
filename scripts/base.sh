1cb7d96319c963079441f79cf7b2bc01e19e9f27
Tobias Bähr <tobias.baehr@reinblau.de>
2014-02-21 16:16:21 +0100

    first commit

---
+++ scripts/base.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Update the box
+apt-get -y update
+apt-get -y install curl
+
+# Set up sudo
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=de_DE"
+EOF
+
+update-grub
+
