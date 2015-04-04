d79f25a282cd7da3245c6017244b4b89393b7a42
tobiasb <tobias.baehr@reinblau.de>
2015-04-04 19:00:07 +0200

    New base box. Debian Jessie rc2.

---
+++ scripts/install_software.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing Docker from get.docker.com"
+echo "------------------------------------"
+echo
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker vagrant
+
+echo
+echo "Installing crane"
+echo "------------------------------------"
+echo
+export VERSION=1.1.1
+bash -c "`curl -sL https://raw.githubusercontent.com/michaelsauter/crane/master/download.sh`" && mv crane /usr/local/bin/crane
+
+echo
+echo "Installing docker-composer"
+echo "------------------------------------"
+echo
+curl -L https://github.com/docker/compose/releases/download/1.2.0rc3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
