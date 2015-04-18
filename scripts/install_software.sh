7cef66bab191e5e2266751db1555c0fb250ec6da
tobiasb <tobias.baehr@reinblau.de>
2015-04-18 12:15:49 +0200

    Updated docker-compose 1.2.0rc3 -> 1.2.0

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -17,6 +17,6 @@ echo
 echo "Installing docker-composer"
 echo "------------------------------------"
 echo
-curl -L https://github.com/docker/compose/releases/download/1.2.0rc3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
