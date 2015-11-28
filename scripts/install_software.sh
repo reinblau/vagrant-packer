b78adfbf19ba12515da113df485781d385424034
tobiasb <tobias.baehr@reinblau.de>
2015-11-28 18:50:36 +0100

    Updated docker installation. Updated docker-compose to latest version.

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -o errexit -o nounset
 
-echo "Installing Docker from get.docker.com"
+# https://docs.docker.com/engine/installation/debian/
+echo "Installing Docker"
 echo "------------------------------------"
 echo
-curl -sSL https://get.docker.com/ | sh
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine=1.9.1-0~jessie
 usermod -aG docker vagrant
 systemctl enable docker
 
@@ -12,7 +16,7 @@ echo
 echo "Installing docker-composer"
 echo "------------------------------------"
 echo
-curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
