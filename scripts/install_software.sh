13fd0396edb20b8f180c761a3a4a85e5d0ff8898
tobiasb <tobias.baehr@reinblau.de>
2016-03-19 23:03:16 +0100

    Updated docker/docker compose

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -8,7 +8,7 @@ echo
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y docker-engine=1.10.1-0~jessie
+apt-get install -y docker-engine=1.10.3-0~jessie
 usermod -aG docker vagrant
 systemctl enable docker
 
@@ -16,7 +16,7 @@ echo
 echo "Installing docker-composer"
 echo "------------------------------------"
 echo
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose &&\
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
