7684ddf42062c64108891b5047a923f8d16fa65b
tobiasb <tobias.baehr@reinblau.de>
2017-06-30 14:41:23 +0200

    Updated to debian 9. Updated docker-compose version.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -11,11 +11,6 @@ echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 rm -rf /tmp/*
 rm -rf /var/lib/apt/lists/*
-# Make sure Udev doesn't block our network
-echo "cleaning up udev rules"
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
